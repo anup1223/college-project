049583530cdbfcf55b2501a74eaf5db076dfb09b
Anup Kumar Tiwari <anupkumartiwari020@gmail.com>
2024-12-04 03:11:43 +0530

    / route

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use((req, res, next) => {
 //     res.send(registeredUser);
 // });
 
-
+app.use("/", listingRouter);
 app.use("/listings", listingRouter);
 
 app.use("/listings/:id/reviews", reviewRouter);
